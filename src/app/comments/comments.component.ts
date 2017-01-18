97c467a7eff33cc0a13cfb5d66b52d3647a13112
jdickinson <jdickinson@statebridgecompany.com>
2017-01-18 09:35:37 -0700

    updated for adding new comment not based on  existing.

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -10,7 +10,7 @@ import {Comments} from "./Comments";
 })
 export class CommentsComponent implements OnInit {
 
-  constructor(public cs: CommentsService) { this.showComments = true; this.expando="Collapse";}
+  constructor(public cs: CommentsService) { this.showComments = true; this.expando="Collapse"; this.setupBy = "not set";}
   comments : Array<Comments>;
   comment : Comments;
   LoanID: string;
@@ -20,6 +20,7 @@ export class CommentsComponent implements OnInit {
   inputComment: string;
   inputNote: string;
   inputAdditionalNote: string;
+  setupBy: string;
   submitComments()
   {
     if(this.comments.length > 0) {
@@ -27,6 +28,13 @@ export class CommentsComponent implements OnInit {
       this.comment.comment = this.inputComment;
       this.comment.notes = this.inputNote;
       this.comment.additionalNotes = this.inputAdditionalNote;
+    }
+    else{
+      this.comment = {dateandTime: new Date(), additionalNotes: this.inputAdditionalNote, notes: this.inputNote,
+        comment: this.inputComment, loanID: this.LoanID, daysAdvanceNot: 0, eventFrequency: 0,
+        letterCode: null, sentToAttorney: 0, rightPartyContact: 0, qualityRightPartyContact: 0, exclude: 0,
+        completedBy: null, setupBy: this.setupBy, completionDate: null, recordtype: 0 , department: 0 , retainPermanent:0,
+        dueDate: null, Counter: 0};
     }
     this.cs.Add(this.comment);
   }
