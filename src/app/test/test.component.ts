6483f76f8335cb60ecc7a60a2480ddaf9f3e16a2
jdickinson <jdickinson@statebridgecompany.com>
2017-01-27 10:07:21 -0700

    boot strap added

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,7 +3,7 @@ import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-
+import * as x2js from 'x2js';
 
 @Component({
   selector: 'app-test',
