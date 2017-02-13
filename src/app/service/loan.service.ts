95fe1777ab2907b387bdabac78e0d34ae48bd432
jdickinson <jdickinson@statebridgecompany.com>
2017-02-13 09:28:33 -0700

    created configuration string for the uri to fiWebApi

--- src/app/service/loan.service.ts
+++ src/app/service/loan.service.ts
@@ -16,40 +16,49 @@ import {payment} from "../payments/payment";
 @Injectable()
 export class LoanService {
 
-  constructor(public http: Http){ this.result = "";}
+
+  constructor(public http: Http){ this.result = "";
+    this.baseURI = 'https://localhost/fiwebapi';
+  }
   result: string;
+  baseURI: string;
   public getComments(id: string): Observable<Array<Comments>>
   {
-    let uri = 'https://localhost/fiwebapi/api/Comments/' + id;
+    let uri = this.baseURI + '/api/Comments/' + id;
     return this.http.get(uri)
       .map((res: Response) => res.json());
   }
   public getPayments(id: string): Observable<Array<payment>>
   {
-    let uri = 'https://localhost/fiwebapi/api/Payments/' + id;
+    let uri = this.baseURI + '/api/Payments/' + id;
+
     return this.http.get(uri)
       .map((res: Response) => res.json());
   }
   public getContacts(id: string): Observable<Array<IContact>>{
-    let uri = 'https://localhost/fiwebapi/api/Contacts/' + id;
+
+    let uri = this.baseURI + '/api/Contacts/' + id;
     return this.http.get(uri)
       .map((res: Response) => res.json());
   }
   public getLoan(id: string): Observable<Array<ILoan>>
   {
-    let uri = 'https://localhost/fiwebapi/api/Loans/' + id;
+
+    let uri = this.baseURI + '/api/Loans/' + id;
     return this.http.get(uri)
       .map((res: Response) => res.json());
   }
   public getMailingAddress(id: string) : Observable<Array<IMailingAddress>>
   {
-    let uri = 'https://localhost/fiwebapi/api/MailingAddress/' + id;
+
+    let uri = this.baseURI + '/api/MailingAddress/' + id;
     return this.http.get(uri)
       .map((res: Response) => res.json());
   }
 
   public Add(c: Comments){
-    let uri = 'http://localhost:5055/api/Comments/';
+    //let uri = 'http://localhost:5055/api/Comments/';
+    let uri = this.baseURI + '/api/Comments/';
     this.save(uri,JSON.stringify(c)).subscribe(r => {
       this.result = r;
       console.log(this.result);
